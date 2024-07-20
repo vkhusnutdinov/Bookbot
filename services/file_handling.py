af8ae1ef79d34b07ca4b8041dca1112ef720c804
Valentine Khusnutdinov <dialog.valentine@gmail.com>
2024-07-20 17:41:43 +0800

    adding the main code to file_handling.py

---
+++ services/file_handling.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from pathlib import Path
+
+BOOK_PATH = 'book/book.txt'
+PAGE_SIZE = 1050
+
+book: dict[int, str] = {}
+
+
+# Функция, возвращающая строку с текстом страницы и ее размер
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+    symbols = {'.', ',', '!', '?', ':', ';'}
+    text_page = text[start:] + ' '
+    size = min(len(text_page), size)
+
+    for i in range(size, 0, -1):
+        if text_page[i - 1] in symbols and text_page[i] not in symbols:
+            return text_page[:i], i
+
+
+# Функция, формирующая словарь книги
+def prepare_book(path: str) -> None:
+    with open(path, encoding='utf-8') as f:
+        text, start, i = f.read(), 0, 0
+        while start < len(text):
+            text_page, size = _get_part_text(text, start, PAGE_SIZE)
+            start += size
+            i += 1
+            book[i] = text_page.lstrip()
+
+
+# Вызов функции prepare_book для подготовки книги из текстового файла
+prepare_book(os.path.join(Path(__file__).parent, os.path.normpath(BOOK_PATH)))
